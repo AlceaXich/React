9bd7864205fd939ca61535446f248f981dd8050f
AlceaXich <shirmoc.t05@gmail.com>
2023-12-05 08:58:19 -0500

    Update App

--- Proyectos/00-hola-mundo/src/App.jsx
+++ Proyectos/00-hola-mundo/src/App.jsx
@@ -4,9 +4,9 @@ import { FacebookFollowCard } from './FacebookFollowCard.jsx'
 export function App () {
     return (
         <div className='App'>
-            <FacebookFollowCard username="alceaxich" name="Shirley Oxa"/>
-            <FacebookFollowCard username="alcexich" name="Shirley Oxa"/>
-            <FacebookFollowCard username="alcexich" name="Shirley Oxa"/>
+            <FacebookFollowCard  isFollowing username="alceaxich" name="Shirley Oxa"/>
+            <FacebookFollowCard isFollowing={false} username="alcexich" name="Shirley Oxa"/>
+            <FacebookFollowCard isFollowing={true} username="alcexich" name="Shirley Oxa"/>
         </div>
     )
 }
